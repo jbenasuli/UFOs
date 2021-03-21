1cf9dfedcb95fe127d04ee6355a41bee71850243
James Benasuli <jamiebenasuli@gmail.com>
2021-03-21 15:45:35 -0400

    add buildTable function

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,25 @@
-// // import the data from data.js
-// const tableData = data;
+// import the data from data.js
+const tableData = data;
 
-// // Reference the HTML table using d3
-// var tbody = d3.select("tbody");
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
 
-// Simple JavaScript console.log statement
-function printHello() {
-    console.log("Hello there!");
-  }
+function buildTable(data) {
+    // clear out existing data
+    tbody.html("");
 
-// function buildTable(data) {
-//   tbody.html("");
-// }
+  // loop through each object
+  //append a row and cell for each value in the row of data
+  data.forEach((dataRow) => {
+    // append a row to the table body
+    let row = tbody.append("tr");
+
+    // loop through each field in dataRow
+    // add each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+    );
+});
+}
